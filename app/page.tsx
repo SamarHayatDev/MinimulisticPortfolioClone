23cd9e652d6328a7a3f23ba0df71ec3274458b73
samar hayat <samarraja228@gmail.com>
2024-05-23 16:25:33 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className=" max-w-7xl w-full">
+        <h1>Hello Portfolio!</h1>
+      </div>
+    </main>
+  );
+}
