4489a7300578c92666bdcfce19ec0278b4f8e6fe
samar hayat <samarraja228@gmail.com>
2024-05-30 17:03:45 +0500

    43:45

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,9 +5,16 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {[{ title: "title", description: "description", id: 1 }].map((item) => (
-          <BentoGridItem />
-        ))}
+        {[{ title: "title", description: "description", id: 1 }].map(
+          ({ id, title, description }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
