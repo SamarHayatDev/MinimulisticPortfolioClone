8765b3d8cfabdcf579eb7b1c9e5f9f9ce9fe420f
samar hayat <samarraja228@gmail.com>
2024-05-31 12:58:48 +0500

    13307

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProject from "@/components/RecentProject";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <RecentProject />
       </div>
     </main>
   );
