9d67157a82e50a09e7342855824aa4fe03700a7c
samar hayat <samarraja228@gmail.com>
2024-05-31 11:41:42 +0500

    1:23:18

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,122 +1,3 @@
-// import { cn } from "@/utils/cn";
-// import { BackgroundGradientAnimation } from "./GradientBg";
-// import { GlobeDemo } from "./GridGlobe";
-
-// export const BentoGrid = ({
-//   className,
-//   children,
-// }: {
-//   className?: string;
-//   children?: React.ReactNode;
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
-//         className
-//       )}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-// export const BentoGridItem = ({
-//   className,
-//   title,
-//   description,
-//   id,
-//   img,
-//   imgClassName,
-//   titleClassName,
-//   spareImg,
-// }: {
-//   className?: string;
-//   title?: string | React.ReactNode;
-//   description?: string | React.ReactNode;
-//   header?: React.ReactNode;
-//   icon?: React.ReactNode;
-//   id: number;
-
-//   img?: string;
-//   imgClassName?: string;
-//   titleClassName?: string;
-//   spareImg?: string;
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "row-span-1 rounded-3xl relative overflow-hidden  group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
-//         className
-//       )}
-//       style={{
-//         background: "rgb(4 7 29)",
-//         backgroundColor:
-//           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-//       }}
-//     >
-//       <div className={`${id === 6} && flex justify-center h-full`}>
-//         <div className=" w-full h-full absolute">
-//           {img && (
-//             <img
-//               src={img}
-//               alt={img}
-//               className={cn(imgClassName, " object-cover object-center")}
-//             />
-//           )}
-//         </div>
-//         <div
-//           className={`absolute right-0 -bottom-5 ${
-//             id === 5 && "w-full opacity-80"
-//           }`}
-//         >
-//           {spareImg && (
-//             <img
-//               src={spareImg}
-//               alt={spareImg}
-//               className={" object-cover object-center w-full h-full"}
-//             />
-//           )}
-//         </div>
-//         {id === 6 && (
-//           <BackgroundGradientAnimation>
-//             <div className=" absolute z-50 flex items-center justify-center text-white font-bold" />
-//           </BackgroundGradientAnimation>
-//         )}
-//         <div
-//           className={cn(
-//             titleClassName,
-//             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full mix-h-40 flex flex-col px-5 p-5 lg:p-10"
-//           )}
-//         >
-//           <div className="font-sans font-extralight text-[#c1c3d3] text-sm md:text-xs lg:text-base z-10">
-//             {description}
-//           </div>
-//           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10 ">
-//             {title}
-//           </div>
-
-//           {id === 2 && <GlobeDemo />}
-
-//           {id === 3 && (
-//             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-//               <div className=" flex flex-col gap-3 lg:gap-8">
-//                 {["React.js", "Next.js", "typescript"].map((item) => (
-//                   <span
-//                     key={item}
-//                     className="py-2 lg:p-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
-//                   >
-//                     {item}
-//                   </span>
-//                 ))}
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 "use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
@@ -162,6 +43,7 @@ export const BentoGridItem = ({
   imgClassName,
   titleClassName,
   spareImg,
+  writeText,
 }: {
   className?: string;
   id: number;
@@ -171,23 +53,23 @@ export const BentoGridItem = ({
   imgClassName?: string;
   titleClassName?: string;
   spareImg?: string;
+  writeText?: string;
 }) => {
   const leftLists = ["ReactJS", "Express", "Typescript"];
   const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
 
   const [copied, setCopied] = useState(false);
-
   const defaultOptions = {
     loop: copied,
     autoplay: copied,
-    animationData: animationData,
+    animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
 
   const handleCopy = () => {
-    const text = "samarhayatofficial@gmail.com";
+    const text = "hsu@jsmastery.pro";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -293,16 +175,7 @@ export const BentoGridItem = ({
           {id === 6 && (
             <div className="mt-5 relative">
               <div className={`absolute -bottom-5 right-0`}>
-                <Lottie
-                  options={{
-                    loop: copied,
-                    autoplay: copied,
-                    animationData: animationData,
-                    preserveAspectRatio: "xMidYMid slice",
-                  }}
-                  height={200}
-                  width={400}
-                />
+                <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
               <MagicButton
