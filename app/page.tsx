7952de1a39e1bf09a78a6475b0478db1ca94e0dd
samar hayat <samarraja228@gmail.com>
2024-06-04 17:11:07 +0500

    2:25:33

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import RecentProject from "@/components/RecentProject";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import { Approach } from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <RecentProject />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
