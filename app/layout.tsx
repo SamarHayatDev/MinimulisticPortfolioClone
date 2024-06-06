2838a673fb8ca4c8859c1f0f0db9ec29f0af9115
samar hayat <samarraja228@gmail.com>
2024-06-06 12:06:00 +0500

    vercel deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
