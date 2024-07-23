10865876cdf307b903806511285103481747e8ec
samarhayat.dev <samarhayat.dev@gmail.com>
2024-07-23 10:49:46 +0000

    setting up recentproject.tsx file

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -14,11 +14,11 @@ const RecentProject = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className=" h-[25rem]  flex items-center justify-center sm:h-[41rem] sm:w-[570px] w-[80vw]"
+            className=" h-[25rem]  flex items-center justify-center sm:h-[41rem] sm:w-[400px] w-[80vw]"
           >
             <PinContainer title={link} href={link}>
               {/* <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] overflow-hidden h-[30vh] mb-10"> */}
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden  mb-10">
+              <div className="relative flex items-center justify-center sm:w-[80vw] lg:max-w-[400px] w-[80vw] overflow-hidden  mb-10">
                 <div className=" relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
